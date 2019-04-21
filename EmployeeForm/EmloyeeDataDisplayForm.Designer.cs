23abef1928fbcdc36acd1200897d56d51f45a1dc
Luis Cortez <lac0084@auburn.com>
2019-04-21 13:42:58 -0500

    added employee data form added object ProductionWorker as data source formatted data grid view

---
+++ EmployeeForm/EmloyeeDataDisplayForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace EmployeeForm
+{
+     partial class EmloyeeDataDisplayForm
+     {
+          /// <summary>
+          /// Required designer variable.
+          /// </summary>
+          private System.ComponentModel.IContainer components = null;
+
+          /// <summary>
+          /// Clean up any resources being used.
+          /// </summary>
+          /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+          protected override void Dispose(bool disposing)
+          {
+               if (disposing && (components != null))
+               {
+                    components.Dispose();
+               }
+               base.Dispose(disposing);
+          }
+
+          #region Windows Form Designer generated code
+
+          /// <summary>
+          /// Required method for Designer support - do not modify
+          /// the contents of this method with the code editor.
+          /// </summary>
+          private void InitializeComponent()
+          {
+               this.components = new System.ComponentModel.Container();
+               this.dataGridView1 = new System.Windows.Forms.DataGridView();
+               this.productionWorkerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+               this.employeeNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.employeeNumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.shiftNumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               this.hourlyPayRateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+               ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+               ((System.ComponentModel.ISupportInitialize)(this.productionWorkerBindingSource)).BeginInit();
+               this.SuspendLayout();
+               // 
+               // dataGridView1
+               // 
+               this.dataGridView1.AutoGenerateColumns = false;
+               this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+               this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.employeeNameDataGridViewTextBoxColumn,
+            this.employeeNumberDataGridViewTextBoxColumn,
+            this.shiftNumberDataGridViewTextBoxColumn,
+            this.hourlyPayRateDataGridViewTextBoxColumn});
+               this.dataGridView1.DataSource = this.productionWorkerBindingSource;
+               this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+               this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+               this.dataGridView1.Name = "dataGridView1";
+               this.dataGridView1.Size = new System.Drawing.Size(482, 292);
+               this.dataGridView1.TabIndex = 0;
+               // 
+               // productionWorkerBindingSource
+               // 
+               this.productionWorkerBindingSource.DataSource = typeof(EmployeeForm.ProductionWorker);
+               // 
+               // employeeNameDataGridViewTextBoxColumn
+               // 
+               this.employeeNameDataGridViewTextBoxColumn.DataPropertyName = "EmployeeName";
+               this.employeeNameDataGridViewTextBoxColumn.HeaderText = "EmployeeName";
+               this.employeeNameDataGridViewTextBoxColumn.Name = "employeeNameDataGridViewTextBoxColumn";
+               // 
+               // employeeNumberDataGridViewTextBoxColumn
+               // 
+               this.employeeNumberDataGridViewTextBoxColumn.DataPropertyName = "EmployeeNumber";
+               this.employeeNumberDataGridViewTextBoxColumn.HeaderText = "EmployeeNumber";
+               this.employeeNumberDataGridViewTextBoxColumn.Name = "employeeNumberDataGridViewTextBoxColumn";
+               // 
+               // shiftNumberDataGridViewTextBoxColumn
+               // 
+               this.shiftNumberDataGridViewTextBoxColumn.DataPropertyName = "ShiftNumber";
+               this.shiftNumberDataGridViewTextBoxColumn.HeaderText = "ShiftNumber";
+               this.shiftNumberDataGridViewTextBoxColumn.Name = "shiftNumberDataGridViewTextBoxColumn";
+               // 
+               // hourlyPayRateDataGridViewTextBoxColumn
+               // 
+               this.hourlyPayRateDataGridViewTextBoxColumn.DataPropertyName = "HourlyPayRate";
+               this.hourlyPayRateDataGridViewTextBoxColumn.HeaderText = "HourlyPayRate";
+               this.hourlyPayRateDataGridViewTextBoxColumn.Name = "hourlyPayRateDataGridViewTextBoxColumn";
+               // 
+               // EmloyeeDataDisplayForm
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.AutoSize = true;
+               this.ClientSize = new System.Drawing.Size(482, 292);
+               this.Controls.Add(this.dataGridView1);
+               this.Name = "EmloyeeDataDisplayForm";
+               this.Text = "Employee Data Information Form";
+               ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+               ((System.ComponentModel.ISupportInitialize)(this.productionWorkerBindingSource)).EndInit();
+               this.ResumeLayout(false);
+
+          }
+
+          #endregion
+
+          private System.Windows.Forms.DataGridView dataGridView1;
+          private System.Windows.Forms.DataGridViewTextBoxColumn employeeNameDataGridViewTextBoxColumn;
+          private System.Windows.Forms.DataGridViewTextBoxColumn employeeNumberDataGridViewTextBoxColumn;
+          private System.Windows.Forms.DataGridViewTextBoxColumn shiftNumberDataGridViewTextBoxColumn;
+          private System.Windows.Forms.DataGridViewTextBoxColumn hourlyPayRateDataGridViewTextBoxColumn;
+          private System.Windows.Forms.BindingSource productionWorkerBindingSource;
+     }
+}
