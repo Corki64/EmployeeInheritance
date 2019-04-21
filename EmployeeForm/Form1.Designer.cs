b2b1cd24596a241298046c5fabbaf2b71d9299a4
Luis Cortez <lac0084@auburn.com>
2019-04-21 11:55:06 -0500

    added and named labels to form

--- EmployeeForm/Form1.Designer.cs
+++ EmployeeForm/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EmployeeForm
 {
-     partial class Form1
+     partial class EmployeeFormDataEntry
      {
           /// <summary>
           /// Required designer variable.
@@ -28,13 +28,70 @@
           /// </summary>
           private void InitializeComponent()
           {
-               this.components = new System.ComponentModel.Container();
+               this.nameLabel = new System.Windows.Forms.Label();
+               this.idLabel = new System.Windows.Forms.Label();
+               this.shiftLabel = new System.Windows.Forms.Label();
+               this.payRateLabel = new System.Windows.Forms.Label();
+               this.SuspendLayout();
+               // 
+               // nameLabel
+               // 
+               this.nameLabel.AutoSize = true;
+               this.nameLabel.Location = new System.Drawing.Point(89, 46);
+               this.nameLabel.Name = "nameLabel";
+               this.nameLabel.Size = new System.Drawing.Size(41, 13);
+               this.nameLabel.TabIndex = 0;
+               this.nameLabel.Text = "Name :";
+               // 
+               // idLabel
+               // 
+               this.idLabel.AutoSize = true;
+               this.idLabel.Location = new System.Drawing.Point(89, 90);
+               this.idLabel.Name = "idLabel";
+               this.idLabel.Size = new System.Drawing.Size(64, 13);
+               this.idLabel.TabIndex = 1;
+               this.idLabel.Text = "ID Number :";
+               // 
+               // shiftLabel
+               // 
+               this.shiftLabel.AutoSize = true;
+               this.shiftLabel.Location = new System.Drawing.Point(89, 136);
+               this.shiftLabel.Name = "shiftLabel";
+               this.shiftLabel.Size = new System.Drawing.Size(70, 13);
+               this.shiftLabel.TabIndex = 2;
+               this.shiftLabel.Text = "Shift (1 or 2) :";
+               // 
+               // payRateLabel
+               // 
+               this.payRateLabel.AutoSize = true;
+               this.payRateLabel.Location = new System.Drawing.Point(89, 198);
+               this.payRateLabel.Name = "payRateLabel";
+               this.payRateLabel.Size = new System.Drawing.Size(87, 13);
+               this.payRateLabel.TabIndex = 3;
+               this.payRateLabel.Text = "Pay Rate (0.00) :";
+               // 
+               // EmployeeFormDataEntry
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
                this.ClientSize = new System.Drawing.Size(800, 450);
+               this.Controls.Add(this.payRateLabel);
+               this.Controls.Add(this.shiftLabel);
+               this.Controls.Add(this.idLabel);
+               this.Controls.Add(this.nameLabel);
+               this.Name = "EmployeeFormDataEntry";
                this.Text = "Form1";
+               this.ResumeLayout(false);
+               this.PerformLayout();
+
           }
 
           #endregion
+
+          private System.Windows.Forms.Label nameLabel;
+          private System.Windows.Forms.Label idLabel;
+          private System.Windows.Forms.Label shiftLabel;
+          private System.Windows.Forms.Label payRateLabel;
      }
 }
 
--- EmployeeForm/Form1.cs
+++ EmployeeForm/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace EmployeeForm
 {
-     public partial class Form1 : Form
+     public partial class EmployeeFormDataEntry : Form
      {
-          public Form1()
+          public EmployeeFormDataEntry()
           {
                InitializeComponent();
           }
